22df1a839f73953650f8c99dd2937bdfce4e87c3
rlarkdals579 <rlarkdals579@naver.com>
2022-07-05 11:44:14 +0900

    TDD test

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,35 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void 더하기() {
+        int rs = Calculator.run("10 + 20");
+
+        assertEquals(30, rs);
+    }
+
+    @Test
+    public void 더하기_2() {
+        int rs = Calculator.run("20 + 20");
+
+        assertEquals(40, rs);
+    }
+
+    @Test
+    public void 더하기_3() {
+        int rs = Calculator.run("10 + 10");
+
+        assertEquals(20, rs);
+    }
+
+    @Test
+    public void 빼기() {
+        int rs = Calculator.run("10 - 10");
+
+        assertEquals(0, rs);
+    }
+}
